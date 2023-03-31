374114a693da2650b0a719ddebcd4bde63fd9765
Eleonore Stultjens <eleonore.jens@gmail.com>
2023-03-31 16:50:10 +0200

    essai de creation de reservation et updateDisponibilite

--- migrations/Version20230331114238.php
+++ migrations/Version20230331114238.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20230331071757 extends AbstractMigration
+final class Version20230331114238 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -23,20 +23,17 @@ final class Version20230331071757 extends AbstractMigration
         $this->addSql('CREATE TABLE address (id INT AUTO_INCREMENT NOT NULL, street VARCHAR(255) DEFAULT NULL, number VARCHAR(10) DEFAULT NULL, box VARCHAR(10) DEFAULT NULL, city VARCHAR(255) DEFAULT NULL, state VARCHAR(255) DEFAULT NULL, postal_code VARCHAR(15) DEFAULT NULL, country VARCHAR(255) DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE categorie (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE complain (id INT AUTO_INCREMENT NOT NULL, user_id INT NOT NULL, object VARCHAR(255) DEFAULT NULL, message LONGTEXT DEFAULT NULL, date_complain DATE DEFAULT NULL, INDEX IDX_2DD0CD6BA76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE detail_reservation (id INT AUTO_INCREMENT NOT NULL, service_id INT NOT NULL, reservation_id INT NOT NULL, begin_date DATE NOT NULL, end_date DATE NOT NULL, INDEX IDX_2DC32404ED5CA9E6 (service_id), INDEX IDX_2DC32404B83297E7 (reservation_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE disponibilite (id INT AUTO_INCREMENT NOT NULL, host_id INT NOT NULL, service_id INT NOT NULL, begin_date_dispo DATE DEFAULT NULL, end_date_dispo DATE DEFAULT NULL, INDEX IDX_2CBACE2F1FB8D185 (host_id), INDEX IDX_2CBACE2FED5CA9E6 (service_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE feedback (id INT AUTO_INCREMENT NOT NULL, refugee_id INT NOT NULL, host_id INT NOT NULL, reservation_id INT NOT NULL, stars INT NOT NULL, comment LONGTEXT DEFAULT NULL, INDEX IDX_D22944587ECD7344 (refugee_id), INDEX IDX_D22944581FB8D185 (host_id), INDEX IDX_D2294458B83297E7 (reservation_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE language (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE language_user (language_id INT NOT NULL, user_id INT NOT NULL, INDEX IDX_BF9A3C0582F1BAF4 (language_id), INDEX IDX_BF9A3C05A76ED395 (user_id), PRIMARY KEY(language_id, user_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE picture (id INT AUTO_INCREMENT NOT NULL, service_id INT NOT NULL, name VARCHAR(255) NOT NULL, updated_at DATE NOT NULL, INDEX IDX_16DB4F89ED5CA9E6 (service_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE reservation (id INT AUTO_INCREMENT NOT NULL, host_id INT NOT NULL, refugee_id INT NOT NULL, date_reservation DATE DEFAULT NULL, code_reservation VARCHAR(255) DEFAULT NULL, resume LONGTEXT DEFAULT NULL, INDEX IDX_42C849551FB8D185 (host_id), INDEX IDX_42C849557ECD7344 (refugee_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE reservation (id INT AUTO_INCREMENT NOT NULL, host_id INT NOT NULL, refugee_id INT NOT NULL, service_id INT NOT NULL, date_reservation DATE DEFAULT NULL, code_reservation VARCHAR(255) DEFAULT NULL, resume LONGTEXT DEFAULT NULL, begin_date DATE NOT NULL, end_date DATE NOT NULL, INDEX IDX_42C849551FB8D185 (host_id), INDEX IDX_42C849557ECD7344 (refugee_id), INDEX IDX_42C84955ED5CA9E6 (service_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE service (id INT AUTO_INCREMENT NOT NULL, categorie_id INT DEFAULT NULL, address_id INT DEFAULT NULL, name VARCHAR(255) NOT NULL, description LONGTEXT NOT NULL, INDEX IDX_E19D9AD2BCF5E72D (categorie_id), INDEX IDX_E19D9AD2F5B7AF75 (address_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) NOT NULL, roles LONGTEXT NOT NULL COMMENT \'(DC2Type:json)\', password VARCHAR(255) NOT NULL, last_name VARCHAR(255) DEFAULT NULL, first_name VARCHAR(255) DEFAULT NULL, phone_number VARCHAR(255) DEFAULT NULL, birth_date DATE DEFAULT NULL, presentation LONGTEXT DEFAULT NULL, picture VARCHAR(255) DEFAULT NULL, gender VARCHAR(30) DEFAULT NULL, discr VARCHAR(255) NOT NULL, national_number_id VARCHAR(255) DEFAULT NULL, status VARCHAR(255) DEFAULT NULL, UNIQUE INDEX UNIQ_8D93D649E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE host_address (host_id INT NOT NULL, address_id INT NOT NULL, INDEX IDX_3AB243B01FB8D185 (host_id), INDEX IDX_3AB243B0F5B7AF75 (address_id), PRIMARY KEY(host_id, address_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE messenger_messages (id BIGINT AUTO_INCREMENT NOT NULL, body LONGTEXT NOT NULL, headers LONGTEXT NOT NULL, queue_name VARCHAR(190) NOT NULL, created_at DATETIME NOT NULL, available_at DATETIME NOT NULL, delivered_at DATETIME DEFAULT NULL, INDEX IDX_75EA56E0FB7336F0 (queue_name), INDEX IDX_75EA56E0E3BD61CE (available_at), INDEX IDX_75EA56E016BA31DB (delivered_at), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE complain ADD CONSTRAINT FK_2DD0CD6BA76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
-        $this->addSql('ALTER TABLE detail_reservation ADD CONSTRAINT FK_2DC32404ED5CA9E6 FOREIGN KEY (service_id) REFERENCES service (id)');
-        $this->addSql('ALTER TABLE detail_reservation ADD CONSTRAINT FK_2DC32404B83297E7 FOREIGN KEY (reservation_id) REFERENCES reservation (id)');
         $this->addSql('ALTER TABLE disponibilite ADD CONSTRAINT FK_2CBACE2F1FB8D185 FOREIGN KEY (host_id) REFERENCES user (id)');
         $this->addSql('ALTER TABLE disponibilite ADD CONSTRAINT FK_2CBACE2FED5CA9E6 FOREIGN KEY (service_id) REFERENCES service (id)');
         $this->addSql('ALTER TABLE feedback ADD CONSTRAINT FK_D22944587ECD7344 FOREIGN KEY (refugee_id) REFERENCES user (id)');
@@ -47,6 +44,7 @@ final class Version20230331071757 extends AbstractMigration
         $this->addSql('ALTER TABLE picture ADD CONSTRAINT FK_16DB4F89ED5CA9E6 FOREIGN KEY (service_id) REFERENCES service (id)');
         $this->addSql('ALTER TABLE reservation ADD CONSTRAINT FK_42C849551FB8D185 FOREIGN KEY (host_id) REFERENCES user (id)');
         $this->addSql('ALTER TABLE reservation ADD CONSTRAINT FK_42C849557ECD7344 FOREIGN KEY (refugee_id) REFERENCES user (id)');
+        $this->addSql('ALTER TABLE reservation ADD CONSTRAINT FK_42C84955ED5CA9E6 FOREIGN KEY (service_id) REFERENCES service (id)');
         $this->addSql('ALTER TABLE service ADD CONSTRAINT FK_E19D9AD2BCF5E72D FOREIGN KEY (categorie_id) REFERENCES categorie (id)');
         $this->addSql('ALTER TABLE service ADD CONSTRAINT FK_E19D9AD2F5B7AF75 FOREIGN KEY (address_id) REFERENCES address (id)');
         $this->addSql('ALTER TABLE host_address ADD CONSTRAINT FK_3AB243B01FB8D185 FOREIGN KEY (host_id) REFERENCES user (id) ON DELETE CASCADE');
@@ -57,8 +55,6 @@ final class Version20230331071757 extends AbstractMigration
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE complain DROP FOREIGN KEY FK_2DD0CD6BA76ED395');
-        $this->addSql('ALTER TABLE detail_reservation DROP FOREIGN KEY FK_2DC32404ED5CA9E6');
-        $this->addSql('ALTER TABLE detail_reservation DROP FOREIGN KEY FK_2DC32404B83297E7');
         $this->addSql('ALTER TABLE disponibilite DROP FOREIGN KEY FK_2CBACE2F1FB8D185');
         $this->addSql('ALTER TABLE disponibilite DROP FOREIGN KEY FK_2CBACE2FED5CA9E6');
         $this->addSql('ALTER TABLE feedback DROP FOREIGN KEY FK_D22944587ECD7344');
@@ -69,6 +65,7 @@ final class Version20230331071757 extends AbstractMigration
         $this->addSql('ALTER TABLE picture DROP FOREIGN KEY FK_16DB4F89ED5CA9E6');
         $this->addSql('ALTER TABLE reservation DROP FOREIGN KEY FK_42C849551FB8D185');
         $this->addSql('ALTER TABLE reservation DROP FOREIGN KEY FK_42C849557ECD7344');
+        $this->addSql('ALTER TABLE reservation DROP FOREIGN KEY FK_42C84955ED5CA9E6');
         $this->addSql('ALTER TABLE service DROP FOREIGN KEY FK_E19D9AD2BCF5E72D');
         $this->addSql('ALTER TABLE service DROP FOREIGN KEY FK_E19D9AD2F5B7AF75');
         $this->addSql('ALTER TABLE host_address DROP FOREIGN KEY FK_3AB243B01FB8D185');
@@ -76,7 +73,6 @@ final class Version20230331071757 extends AbstractMigration
         $this->addSql('DROP TABLE address');
         $this->addSql('DROP TABLE categorie');
         $this->addSql('DROP TABLE complain');
-        $this->addSql('DROP TABLE detail_reservation');
         $this->addSql('DROP TABLE disponibilite');
         $this->addSql('DROP TABLE feedback');
         $this->addSql('DROP TABLE language');
--- src/Controller/DisponibiliteController.php
+++ src/Controller/DisponibiliteController.php
@@ -3,101 +3,49 @@
 namespace App\Controller;
 
 use DateTime;
-use Faker\Factory;
-use App\Entity\Host;
-use App\Entity\Refugee;
-use App\Entity\Service;
 use App\Entity\Reservation;
 use App\Entity\Disponibilite;
-use Doctrine\ORM\EntityManager;
-use App\Entity\DetailReservation;
 use Doctrine\ORM\EntityManagerInterface;
-use App\Repository\ReservationRepository;
-use App\Repository\DisponibiliteRepository;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Validator\Constraints\Date;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-class ReservationController extends AbstractController
+class DisponibiliteController extends AbstractController
 {
-    #[Route('/reservations', name: 'user_reservation')]
-    public function getReservationsByUser(ReservationRepository $repo, EntityManagerInterface $em): Response
+    #[Route('/disponibilite', name: 'app_disponibilite')]
+    public function index(): Response
     {
-        // $reservations = $findByUser();
-
-        return $this->render('reservation/user_reservations.html.twig', [
-                    'controller_name' => 'ReservationController',
-                ]);
+        return $this->render('disponibilite/index.html.twig', [
+            'controller_name' => 'DisponibiliteController',
+        ]);
     }
 
-    #[Route('/reservation', name: 'create_reservation')]
-    public function createReservation(EntityManagerInterface $em, DisponibiliteRepository $dispoRepo, Request $req): Response
-    {
-        $faker = Factory::create();
-
-        $idCategorie = (int)$req->get('disponibilite'); 
-        $disponibilite = $dispoRepo->find($idCategorie);
-
-        $reservation = new Reservation();
-        $reservation->setDateReservation(new DateTime('now'));
-
-        // s'il existe une endDate nous sommes dans le cas d'un Accommodation avec un créneau
-        $beginDate = new DateTime($req->get('beginDate'));
-        if ($req->get('endDate')){
-            $endDate = new DateTime($req->get('endDate'));
-            $reservation->setEndDate($endDate);
-        }
-        // nous sommes dans le cas d'un service qui ne se réserve que sur une seule date (endDate = beginDate)
-        else {
-            $reservation->setEndDate($beginDate);
-        }
-        $reservation->setBeginDate($beginDate);
-        $reservation->setCodeReservation($faker->regexify('[A-Z]{5}[0-4]{3}'));
-        $reservation->setService($disponibilite->getService());
-        $reservation->setHost($disponibilite->getHost());
-        $reservation->setRefugee($this->getUser());
-
-        $em->persist($reservation);
-        $em->flush();
-
-        // appeller la fonction de mise à jour du tableau de disponibilités ! 
-        $this->updateDisponibilite($disponibilite, $reservation, $em); 
-
-        // renvoyer vers toutes les réservations du Refugee
-        // Donc doit lancer une action index
-        return $this->redirectToRoute('user_reservation');
-    }
-
-
-     public function updateDisponibilite(Disponibilite $dispo, Reservation $res, EntityManagerInterface $em)
-    {
-        // $service = $em->getRepository(Service::class)->find(1);
-        // $host = $em->getRepository(Host::class)->find(1);
-        // $refugee = $em->getRepository(Refugee::class)->find(5);
+    public function updateDisponibilite(EntityManagerInterface $em) {
+        $service = $em->getRepository(Service::class)->find(1);
+        $host = $em->getRepository(Host::class)->find(1);
+        $refugee = $em->getRepository(Refugee::class)->find(5);
         
         // créer une disponibilité
-        // $dispo = new Disponibilite();
-        // $dispo->setHost($reservation->getHost());
-        // $dispo->setService($reservation->getService());
-        // $dispo->setBeginDateDispo(new DateTime("01-01-2023"));
-        // $dispo->setEndDateDispo(new DateTime("07-01-2023"));
-        // $em->persist($dispo);
-        // $em->flush();
-        // $dispoOriginale = $em->getRepository(Disponibilite::class)->find(1);
+        $dispo = new Disponibilite();
+        $dispo->setHost($host);
+        $dispo->setService($service);
+        $dispo->setBeginDateDispo(new DateTime("01-01-2023"));
+        $dispo->setEndDateDispo(new DateTime("07-01-2023"));
+        $em->persist($dispo);
+        $em->flush();
+        $dispoOriginale = $em->getRepository(Disponibilite::class)->find(1);
         
         // créer une réservation 
-        // $res = new Reservation();
-        // $res->setHost($host);
-        // $res->setRefugee($refugee);
-        // $res->setDateReservation(new DateTime("22-10-2022"));
-        // $res->setCodeReservation("AB123");
-        // $res->setService($service);
-        // $res->setBeginDate(new DateTime("01-01-2023"));
-        // $res->setEndDate(new DateTime("04-01-2023"));
-        // $em->persist($res);
-        // $em->flush();
+        $res = new Reservation();
+        $res->setHost($host);
+        $res->setRefugee($refugee);
+        $res->setDateReservation(new DateTime("22-10-2022"));
+        $res->setCodeReservation("AB123");
+        $res->setService($service);
+        $res->setBeginDate(new DateTime("01-01-2023"));
+        $res->setEndDate(new DateTime("04-01-2023"));
+        $em->persist($res);
+        $em->flush();
 
         //TEST DE DIFFERENTES DATES
         // $res->setBeginDate(new DateTime("03-01-2023"));
@@ -129,7 +77,7 @@ class ReservationController extends AbstractController
 
         if ($debutReservation == $debutDispo  && $finReservation == $finDispo) {
             // - Effacer disponibilité du tableau, tout es pris
-            $em->remove($dispo);
+            $em->remove($dispoOriginale);
         } else {
 
             // 2. La réservation commence après le début de la disponibilité et/ou finit avant la fin de la disponibilité
@@ -150,8 +98,8 @@ class ReservationController extends AbstractController
                 $creneau1->setBeginDateDispo($debutDispo); // date original de dispo
                 // $creneau1->setEndDateDispo($debutReservation->modify('-1 day')); // le prémier créneau finit le jour avant que la Reservation commence 
                 $creneau1->setEndDateDispo($debutReservation);
-                $creneau1->setService($dispo->getService());
-                $creneau1->setHost($dispo->getHost());
+                $creneau1->setService($service);
+                $creneau1->setHost($host);
                 $em->persist($creneau1);
                 // $em->remove($dispoOriginale);
             }
@@ -165,9 +113,9 @@ class ReservationController extends AbstractController
                 // $creneau2->setBeginDateDispo($finReservation->modify('+1 day')); // dispo 1 jour après la fin de la réservation
                 $creneau2->setBeginDateDispo($finReservation);
                 $creneau2->setEndDateDispo($finDispo); // dispo jusqu'à la fin de la disponibilité
-                $creneau2->setService($dispo->getService());
+                $creneau2->setService($service);
                 // dd($host);
-                $creneau2->setHost($dispo->getHost());
+                $creneau2->setHost($host);
                 $em->persist($creneau2);
                 // $em->remove($dispoOriginale);
             }
@@ -175,7 +123,7 @@ class ReservationController extends AbstractController
         // on aurait pu factoriser le code et mettre un seul remove ici 
         // (dans tous les cas il faut effacer la dispo originale!)
         // au lieu de dans chaque cas de figure. C'est juste pour l'explication qu'on les a mis en haut
-        $em->remove ($dispo);
+        $em->remove ($dispoOriginale);
         $em->flush();
     }
 }
