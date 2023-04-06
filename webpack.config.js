5c3154e421ff5091e5b0376c61ebacc00a4db86b
Eleonore Stultjens <eleonore.jens@gmail.com>
2023-04-06 16:17:35 +0200

    setpublicpath

--- webpack.config.js
+++ webpack.config.js
@@ -11,9 +11,9 @@ if (!Encore.isRuntimeEnvironmentConfigured()) {
 Encore
 
     // directory where compiled assets will be stored
-    .setOutputPath('public/build/')
+    .setOutputPath('/public/build/')
     // public path used by the web server to access the output path
-    .setPublicPath('/build')
+    .setPublicPath('/project1/public/build')
     // only needed for CDN's or subdirectory deploy
     //.setManifestKeyPrefix('build/')
 
